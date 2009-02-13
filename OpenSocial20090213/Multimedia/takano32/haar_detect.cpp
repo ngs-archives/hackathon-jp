1d89b2d8ec3e95a8b06d124f858c6cd5ae81d267
takano32 <takano32@bce7ee96-cdb0-11dd-aa7b-9fdd872f0417>
2009-02-13 06:58:32 +0000

    trivial commit

--- OpenSocial20090213/Multimedia/takano32/haar_detect.cpp
+++ OpenSocial20090213/Multimedia/takano32/haar_detect.cpp
@@ -6,6 +6,7 @@ int
 main (int argc, char **argv)
 {
 	int i;
+	int gui = true;
 	IplImage *src_img = 0, *src_gray = 0;
 	const char *cascade_name = "/usr/share/opencv/haarcascades/haarcascade_frontalface_default.xml";
 	CvHaarClassifierCascade *cascade = 0;
@@ -21,6 +22,7 @@ main (int argc, char **argv)
 	// (1)画像を読み込む
 	if (argc < 2 || (src_img = cvLoadImage (argv[1], CV_LOAD_IMAGE_COLOR)) == 0)
 		return -1;
+	
 	src_gray = cvCreateImage (cvGetSize (src_img), IPL_DEPTH_8U, 1);
 
 	// (2)ブーストされた分類器のカスケードを読み込む
