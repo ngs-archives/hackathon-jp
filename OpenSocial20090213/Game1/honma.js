494fe33ecf32b930609f6dbedcf34d1d181ee35c
hira.tara <hira.tara@bce7ee96-cdb0-11dd-aa7b-9fdd872f0417>
2009-02-13 06:40:04 +0000

    メイン関数を移動(すみません

--- OpenSocial20090213/Game1/honma.js
+++ OpenSocial20090213/Game1/honma.js
@@ -104,15 +104,3 @@ socialquest.recievedViewerData = function (data){
 
 })();
 // ============================================================================
-
-gadgets.util.registerOnLoadHandler(
-    function () {
-        var req = opensocial.newDataRequest();
-        req.add(
-            req.newFetchPersonRequest(
-                "VIEWER", socialquest.parsonSearchParams
-            ), "viewer"
-        );
-        req.send(socialquest.recievedViewerData);
-    }
-);
