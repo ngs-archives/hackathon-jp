73b45c40e91359367f14334054a0ffff89daa280
ago.kyo <ago.kyo@bce7ee96-cdb0-11dd-aa7b-9fdd872f0417>
2009-02-13 06:02:39 +0000

    add vds

--- OpenSocial20090213/Multimedia/ago/canvas.js
+++ OpenSocial20090213/Multimedia/ago/canvas.js
@@ -2,17 +2,21 @@
  * big fonts!
  * This JavaScript file is for Canvas view.
  */
-var $os = $.opensocial_simple;
-$os.getPerson(function (person) {
-	$os.get('http://profile.myspace.com/index.cfm', {
-		'fuseaction' : 'user.viewProfile',
-		'friendID' : person.OWNER.getId().replace(/\D/g, '')
-	}, function (data) {
-		var text = '';
-		$(data).find('.latestBlogEntry:first .text').each(function () {
-			text += $(this).html();
+$(function () {
+	var $os = $.opensocial_simple;
+	vds = new VoiceDelivery((new VoiceDeliveryPlayer("vdsp")), "vds");
+	$os.getPerson(function (person) {
+		$os.get('http://profile.myspace.com/index.cfm', {
+			'fuseaction' : 'user.viewProfile',
+			'friendID' : person.OWNER.getId().replace(/\D/g, '')
+		}, function (data) {
+			var text = '';
+			$(data).find('.latestBlogEntry:first .text').each(function () {
+				text += $(this).html();
+			});
+			$('#textFrontBg').before('<div id="bigFontText" style="font-size:300px; color:#F30">'+text+'</div>');
+			new Marquee('bigFontText', {delay:25});
+			vds.speak(text);
 		});
-		$('#textFrontBg').before('<div id="bigFontText" style="font-size:300px; color:#F30">'+text+'</div>');
-		new Marquee('bigFontText');
 	});
 });
