f765318501b7ffd4b52290f53298d22d48398a0d
masaki.sw <masaki.sw@bce7ee96-cdb0-11dd-aa7b-9fdd872f0417>
2009-02-13 04:12:50 +0000

    go

--- OpenSocial20090213/Feed/sawamura/test.js
+++ OpenSocial20090213/Feed/sawamura/test.js
@@ -8,6 +8,7 @@ function show(feed){
     html.push("</h1>");
     $.each(feed.data.Entry,function(data){
 	    html.push("<h2>" + this.Link + "</h2>");
+	    html.push("<p>" + this.Summary + "</p>");
 	});
     document.getElementById('friends').innerHTML += html.join('');
 }
