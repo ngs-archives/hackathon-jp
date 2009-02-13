f135276d39669a59c66de2c6619276511dc6f8fb
keisuke.amaya <keisuke.amaya@bce7ee96-cdb0-11dd-aa7b-9fdd872f0417>
2009-02-13 04:47:02 +0000

    add get_booklog

---
+++ OpenSocial20090213/Feed/amaya/test.js
@@ -0,0 +1,66 @@
+
+gadgets.util.registerOnLoadHandler(init);
+
+
+function txt(dom,tag){
+    return dom.getElementsByTagName(tag)[0].childNodes[0].nodeValue;
+}
+
+    
+function show(oj){ 
+    var html = new Array();
+    html.push("<h1>");
+    var dom = oj.data;
+    html.push( txt(dom,"title") );
+    html.push("</h1>");
+    var items = dom.getElementsByTagName("item");
+    $.each(items,function(){
+	    html.push("<h2>" + txt(this,"title") + "</h2>");
+	    var cdata = txt(this,"description");
+	    cdata.match(/img src="(.+?)"/);
+	    var img = RegExp.$1;
+	    cdata.match(/asin\/(\w+)/);
+	    var asin = RegExp.$1;
+	    //html.push("<blockquote>" + cdata + "</blockquote>");
+	    html.push("<img src='" + img + "' width='80' />");
+	    html.push("ASIN: " + asin );
+	});
+    document.getElementById('friends').innerHTML += html.join('');
+}
+
+var params = {};
+params[gadgets.io.RequestParameters.METHOD] = gadgets.io.MethodType.GET;
+params[gadgets.io.RequestParameters.CONTENT_TYPE] = gadgets.io.ContentType.DOM;
+
+gadgets.io.makeRequest("http://booklog.jp/users/amaya/feed/RSS1", show, params );
+			  
+function init() {
+    loadFriends();
+}
+
+function loadFriends() {
+    var req = opensocial.newDataRequest();
+    req.add(req.newFetchPersonRequest(opensocial.IdSpec.PersonId.VIEWER), 'viewer');
+    
+    var viewerFriends = opensocial.newIdSpec({ "userId" : "VIEWER", "groupId" : "FRIENDS" });
+    var opt_params = {};
+    opt_params[opensocial.DataRequest.PeopleRequestFields.MAX] = 100;
+    req.add(req.newFetchPeopleRequest(viewerFriends, opt_params), 'viewerFriends');
+    
+    req.send(onLoadFriends);
+}
+
+function onLoadFriends(data) {
+    var viewer = data.get('viewer').getData();
+    var viewerFriends = data.get('viewerFriends').getData();
+    
+    html = new Array();
+    html.push('<ul>');
+    viewerFriends.each(function(person) {
+	    if (person.getId()) {
+		html.push('<li>: ' + person.getDisplayName() + "</li>");
+	    }
+	});
+    html.push('</ul>');
+    //document.getElementById('friends').innerHTML = html.join('');
+}
