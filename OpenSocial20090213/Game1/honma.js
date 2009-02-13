95e0f078a823c9cb0a3ecae41015611f5cea612a
hira.tara <hira.tara@bce7ee96-cdb0-11dd-aa7b-9fdd872f0417>
2009-02-13 04:24:30 +0000

    orcutで対応している項目一覧を追加

--- OpenSocial20090213/Game1/honma.js
+++ OpenSocial20090213/Game1/honma.js
@@ -1,6 +1,43 @@
 var socialquest = new Object();
 
 // ============================================================================
+socialquest.parsonSearchParams = new Object();
+socialquest.parsonSearchParams[
+	opensocial.DataRequest.PeopleRequestFields
+] = [
+    opensocial.Person.Field.ID,
+    opensocial.Person.Field.NAME,
+    opensocial.Person.Field.THUMBNAIL_URL,
+    opensocial.Person.Field.PROFILE_URL,
+    opensocial.Person.Field.CURRENT_LOCATION,
+    opensocial.Person.Field.ADDRESSES,
+    opensocial.Person.Field.ABOUT_ME,
+    opensocial.Person.Field.STATUS,
+    opensocial.Person.Field.GENDER,
+    opensocial.Person.Field.RELATIONSHIP_STATUS,
+    opensocial.Person.Field.LIVING_ARRANGEMENT,
+    opensocial.Person.Field.LANGUAGES_SPOKEN,
+    opensocial.Person.Field.JOB_INTERESTS,
+    opensocial.Person.Field.INTERESTS,
+    opensocial.Person.Field.URLS,
+    opensocial.Person.Field.MUSIC,
+    opensocial.Person.Field.MOVIES,
+    opensocial.Person.Field.TV_SHOWS,
+    opensocial.Person.Field.BOOKS,
+    opensocial.Person.Field.ACTIVITIES,
+    opensocial.Person.Field.SPORTS,
+    opensocial.Person.Field.FOOD,
+    opensocial.Person.Field.TURN_ONS,
+    opensocial.Person.Field.TURN_OFFS,
+    opensocial.Person.Field.ROMANCE,
+    opensocial.Person.Field.FASHION,
+    opensocial.Person.Field.HUMOR,
+    opensocial.Person.Field.LOOKING_FOR,
+    opensocial.Person.Field.RELIGION,
+    opensocial.Person.Field.POLITICAL_VIEWS
+];
+
+
 ( function(){
 socialquest.Player = function (name, hp, sp){
     this.name = name;
@@ -35,7 +72,7 @@ gadgets.util.registerOnLoadHandler(
         var req = opensocial.newDataRequest();
         req.add(
             req.newFetchPersonRequest(
-                "VIEWER"
+                "VIEWER", socialquest.parsonSearchParams
             ), "viewer"
         );
         req.send(socialquest.recievedViewerData);
