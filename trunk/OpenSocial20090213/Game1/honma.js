0f6cc918e28ace6c5604b10bfd0a5d0598097343
hira.tara <hira.tara@bce7ee96-cdb0-11dd-aa7b-9fdd872f0417>
2009-02-13 03:40:00 +0000

    ビューア情報取得処理

--- trunk/OpenSocial20090213/Game1/honma.js
+++ trunk/OpenSocial20090213/Game1/honma.js
@@ -6,9 +6,33 @@ var Player = function (name, hp, sp){
 Player.prototype = {
 };
 
+var CharacterMaker = function (){};
+CharacterMaker.prototype = {
+    make: function (p) {
+        
+        
+    }
+};
+
 
 gadgets.util.registerOnLoadHandler(
     function () {
-        alert("TEST");
+        var req = opensocial.newDataRequest();
+        req.add(
+            req.newFetchPersonRequest(
+                "VIEWER"
+            ), "viewer"
+        );
+        req.send(recievedViewerData);
     }
 );
+
+function recievedViewerData(data){
+    if(data.hadError()){
+        // 例外送出
+        throw data.getErrorMessage();
+    }
+    var p = data.get("viewer").getData();
+    alert(p.getId() + p.getDisplayName());
+}
+
