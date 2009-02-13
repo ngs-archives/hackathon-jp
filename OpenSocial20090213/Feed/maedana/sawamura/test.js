468929b366cfc782cc605d39f0decf1aed1ae901
maeda.na <maeda.na@bce7ee96-cdb0-11dd-aa7b-9fdd872f0417>
2009-02-13 05:33:54 +0000

    動作確認のため

--- OpenSocial20090213/Feed/maedana/sawamura/test.js
+++ OpenSocial20090213/Feed/maedana/sawamura/test.js
@@ -28,35 +28,35 @@ params[gadgets.io.RequestParameters.CONTENT_TYPE] = gadgets.io.ContentType.DOM;
 gadgets.io.makeRequest("http://booklog.jp/users/sawamur/feed/RSS1", show, params );
 
 function init() {
-    loadFriends();
+//    loadFriends();
 }
 
-function loadFriends() {
-    var req = opensocial.newDataRequest();
-    req.add(req.newFetchPersonRequest(opensocial.IdSpec.PersonId.VIEWER), 'viewer');
+//function loadFriends() {
+//    var req = opensocial.newDataRequest();
+//    req.add(req.newFetchPersonRequest(opensocial.IdSpec.PersonId.VIEWER), 'viewer');
+//
+//    var viewerFriends = opensocial.newIdSpec({ "userId" : "VIEWER", "groupId" : "FRIENDS" });
+//    var opt_params = {};
+//    opt_params[opensocial.DataRequest.PeopleRequestFields.MAX] = 100;
+//    req.add(req.newFetchPeopleRequest(viewerFriends, opt_params), 'viewerFriends');
+//
+//    req.send(onLoadFriends);
+//}
 
-    var viewerFriends = opensocial.newIdSpec({ "userId" : "VIEWER", "groupId" : "FRIENDS" });
-    var opt_params = {};
-    opt_params[opensocial.DataRequest.PeopleRequestFields.MAX] = 100;
-    req.add(req.newFetchPeopleRequest(viewerFriends, opt_params), 'viewerFriends');
-
-    req.send(onLoadFriends);
-}
-
-function onLoadFriends(data) {
-    var viewer = data.get('viewer').getData();
-    var viewerFriends = data.get('viewerFriends').getData();
-
-    html = new Array();
-    html.push('<ul>');
-    viewerFriends.each(function(person) {
-        if (person.getId()) {
-        html.push('<li>: ' + person.getDisplayName() + "</li>");
-        }
-    });
-    html.push('</ul>');
-    //document.getElementById('friends').innerHTML = html.join('');
-}
+//function onLoadFriends(data) {
+//    var viewer = data.get('viewer').getData();
+//    var viewerFriends = data.get('viewerFriends').getData();
+//
+//    html = new Array();
+//    html.push('<ul>');
+//    viewerFriends.each(function(person) {
+//        if (person.getId()) {
+//        html.push('<li>: ' + person.getDisplayName() + "</li>");
+//        }
+//    });
+//    html.push('</ul>');
+//    //document.getElementById('friends').innerHTML = html.join('');
+//}
 
 function storeBookData(dom) {
     var req = opensocial.newDataRequest();
