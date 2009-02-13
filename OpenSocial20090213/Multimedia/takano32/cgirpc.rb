57b5d3907090a6707a5c168ed3feaca1a5a1636b
takano32 <takano32@bce7ee96-cdb0-11dd-aa7b-9fdd872f0417>
2009-02-13 08:11:38 +0000

    implement cgi end-point

--- OpenSocial20090213/Multimedia/takano32/cgirpc.rb
+++ OpenSocial20090213/Multimedia/takano32/cgirpc.rb
@@ -1,6 +1,26 @@
 require 'cgi'
 cgi = CGI.new
-uri = cgi.params['image'][0]
+url = cgi.params['image'][0]
+uri = URI.parse(url)
+
+open(uri) do |data|
+	open('face.jpg', 'w') do |file|
+		file.print(data.read)
+	end
+end
+
+
+cgi.out(
+        "type"       => "image/jpeg",
+        "charset"    => "iso-2022-jp"
+        )do
+	IO.popen("./haar_detect face.jpg --no-gui", "r+") do |io|
+		io.read
+	end
+end
+
+
+
 
 
 
