a6b49b78c1f06c4b56b5f3d25f5712d53f1f3ef4
ago.kyo <ago.kyo@bce7ee96-cdb0-11dd-aa7b-9fdd872f0417>
2009-02-13 04:40:32 +0000

    bug fix

--- OpenSocial20090213/Multimedia/ago/canvas.js
+++ OpenSocial20090213/Multimedia/ago/canvas.js
@@ -4,25 +4,19 @@
  */
 
 // TODO: Write the code for Canvas view.
-
-<script type=" text/javascript">
-	var $os = $.opensocial_simple;
-	$os.getPerson(function (person) {
-		$os.getDOM('http://profile.myspace.com/index.cfm', {
-			'fuseaction' : 'user.viewProfile',
-			'friendID' : person.OWNER.getId()
-		}, function (data) {
-			var text = '';
-			$(data).find('.latestBlogEntry:first .text').each(function () {
-				text += this;
-			})
-			$('marquee').css({
-				'font-size' : '1000%',
-				'color' : '#FBEB8C'
-			}).html(text);
+var $os = $.opensocial_simple;
+$os.getPerson(function (person) {
+	$os.get('http://profile.myspace.com/index.cfm', {
+		'fuseaction' : 'user.viewProfile',
+		'friendID' : person.OWNER.getId().replace(/\D/g, '')
+	}, function (data) {
+		var text = '';
+		$(data).find('.latestBlogEntry:first .text').each(function () {
+			text += $(this).html();
 		});
+		$('marquee').css({
+			'font-size' : '1000%',
+			'color' : '#FBEB8C'
+		}).html(text);
 	});
-</script>
-
-<marquee scrollamount="20"></marquee>
-
+});
