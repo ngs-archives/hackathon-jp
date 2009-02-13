9fba8e974fb48d17978b79c1767b10848490f55b
masaki.sw <masaki.sw@bce7ee96-cdb0-11dd-aa7b-9fdd872f0417>
2009-02-13 04:36:46 +0000

    go

--- OpenSocial20090213/Feed/sawamura/test.js
+++ OpenSocial20090213/Feed/sawamura/test.js
@@ -1,11 +1,9 @@
 
 gadgets.util.registerOnLoadHandler(init);
 
-
 function txt(dom,tag){
     return dom.getElementsByTagName(tag)[0].childNodes[0].nodeValue;
 }
-
     
 function show(oj){ 
     var html = new Array();
@@ -33,34 +31,5 @@ params[gadgets.io.RequestParameters.METHOD] = gadgets.io.MethodType.GET;
 params[gadgets.io.RequestParameters.CONTENT_TYPE] = gadgets.io.ContentType.DOM;
 
 gadgets.io.makeRequest("http://booklog.jp/users/sawamur/feed/RSS1", show, params );
-			  
-function init() {
-    loadFriends();
-}
 
-function loadFriends() {
-    var req = opensocial.newDataRequest();
-    req.add(req.newFetchPersonRequest(opensocial.IdSpec.PersonId.VIEWER), 'viewer');
-    
-    var viewerFriends = opensocial.newIdSpec({ "userId" : "VIEWER", "groupId" : "FRIENDS" });
-    var opt_params = {};
-    opt_params[opensocial.DataRequest.PeopleRequestFields.MAX] = 100;
-    req.add(req.newFetchPeopleRequest(viewerFriends, opt_params), 'viewerFriends');
-    
-    req.send(onLoadFriends);
-}
 
-function onLoadFriends(data) {
-    var viewer = data.get('viewer').getData();
-    var viewerFriends = data.get('viewerFriends').getData();
-    
-    html = new Array();
-    html.push('<ul>');
-    viewerFriends.each(function(person) {
-	    if (person.getId()) {
-		html.push('<li>: ' + person.getDisplayName() + "</li>");
-	    }
-	});
-    html.push('</ul>');
-    //document.getElementById('friends').innerHTML = html.join('');
-}
