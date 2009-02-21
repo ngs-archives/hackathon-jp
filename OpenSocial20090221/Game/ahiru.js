df14be5294448d1dd85e1a59bf8f1b80294f97d2
ujihisa <ujihisa@bce7ee96-cdb0-11dd-aa7b-9fdd872f0417>
2009-02-21 03:53:12 +0000

    show viewer friends

--- OpenSocial20090221/Game/ahiru.js
+++ OpenSocial20090221/Game/ahiru.js
@@ -1,23 +1,29 @@
-var loadFriends = function() {
-  var req = opensocial.newDataRequest();
-  req.add(req.newFetchPersonRequest('VIEWER'), 'viewer');
-  req.add(req.newFetchPeopleRequest('VIEWER_FRIENDS'), 'viewerFriends');
-  req.send(onLoadFriends);
-};
-
-function onLoadFriends(data) {
-  var viewer = data.get('viewer').getData();
-  var viewerFriends = data.get('viewerFriends').getData();
-
-  html = new Array();
-  html.push('<ul>');
-  viewerFriends.each(function(person) {
-    html.push('<li>' + person.getDisplayName() + "</li>");
-  });
-  html.push('</ul>');
-  document.getElementById('friends').innerHTML = html.join('');
-}
-
 function init() {
-  loadFriends();
+  var params = {};
+
+  params[opensocial.DataRequest.PeopleRequestFields] = [
+    opensocial.Person.Field.AGE,
+    opensocial.Person.Field.GENDER,
+    opensocial.Person.Field.CURRENT_LOCATION];
+
+  var req = opensocial.newDataRequest();
+  req.add(
+      req.newFetchPersonRequest(opensocial.IdSpec.PersonId.VIEWER_FRIENDS, params),
+      'viewerFriends');
+  req.send(function(data) {
+      var people = data.get('viewerFriends').getData();
+
+      html = new Array();
+      html.push('<ul>');
+      viewerFriends.each(function(person) {
+        var age = person.getField(opensocial.Person.Field.AGE);
+        var gender = person.getField(opensocial.Person.Field.GENDER);
+        var addr = person.getField(opensocial.Person.Field.CURRENT_LOCATION);
+
+        html.push('<li>' + person.getDisplayName() + "(" + age + ")" + "</li>");
+      });
+      html.push('</ul>');
+      document.getElementById('friends').innerHTML = html.join('');
+      });
+  //loadFriends();
 }
