f7949ed5596d474a5f2f77e5087a707520253bea
chris4403 <chris4403@bce7ee96-cdb0-11dd-aa7b-9fdd872f0417>
2009-02-21 03:31:50 +0000

    set test data

--- OpenSocial20090221/Feed/js/UgoSocial.js
+++ OpenSocial20090221/Feed/js/UgoSocial.js
@@ -1,6 +1,35 @@
+var displayData = {
+    memoId : 'ugomemo:XXXXXXXXXXXXX:BBBBBBBBBBBBBBBB',
+    comments : [
+        {
+            time    : 1000,
+            user    : 'chris4403',//TODO fix to userid
+            comment : 'ほげほげ'
+        },
+        {
+            time    : 2000,
+            user    : 'someda',//TODO fix to userid
+            comment : 'もげもげー'
+        },
+        {
+            time    : 3000,
+            user    : 'nakazoe',//TODO fix to userid
+            comment : 'うごうごー'
+        }
+    ]
+}
+
 var UgoSocial = {};
 UgoSocial.showComment = function() {
-    alert('show comment');
+   for ( var p in displayData.comments ) {
+       var data = displayData.comments[p];
+       setTimeout(function(){
+           $('#comment_area').append('<p>' + data.comment + '</p>');
+        },data.time);
+   }
 }
-var us = UgoSocial;
-$('event_trigger').click(us.showComment);
+UgoSocial.init = function() {
+    $('event_trigger').click(us.showComment);
+}
+$(UgoSocial.init);
+
