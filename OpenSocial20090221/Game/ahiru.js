f3b2d2a740ea0b04235e42614c8876c09740850e
ujihisa <ujihisa@bce7ee96-cdb0-11dd-aa7b-9fdd872f0417>
2009-02-21 03:43:26 +0000

    rename and fix

--- OpenSocial20090221/Game/ahiru.js
+++ OpenSocial20090221/Game/ahiru.js
@@ -1,4 +1,11 @@
-var onLoadFriends = function(data) {
+var loadFriends = function() {
+  var req = opensocial.newDataRequest();
+  req.add(req.newFetchPersonRequest('VIEWER'), 'viewer');
+  req.add(req.newFetchPeopleRequest('VIEWER_FRIENDS'), 'viewerFriends');
+  req.send(onLoadFriends);
+};
+
+function onLoadFriends(data) {
   var viewer = data.get('viewer').getData();
   var viewerFriends = data.get('viewerFriends').getData();
 
@@ -9,15 +16,8 @@ var onLoadFriends = function(data) {
   });
   html.push('</ul>');
   document.getElementById('friends').innerHTML = html.join('');
-};
+}
 
-var loadFriends = function() {
-  var req = opensocial.newDataRequest();
-  req.add(req.newFetchPersonRequest('VIEWER'), 'viewer');
-  req.add(req.newFetchPeopleRequest('VIEWER_FRIENDS'), 'viewerFriends');
-  req.send(onLoadFriends);
-};
-
-var init = function() {
+function init() {
   loadFriends();
-};
+}
