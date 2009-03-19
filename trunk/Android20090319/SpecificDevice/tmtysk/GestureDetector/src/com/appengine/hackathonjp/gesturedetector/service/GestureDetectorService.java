39758c6e6791a9f5153f8504ab75739db71aeda9
ogurash <ogurash@bce7ee96-cdb0-11dd-aa7b-9fdd872f0417>
2009-03-19 05:09:31 +0000

    update listener

--- trunk/Android20090319/SpecificDevice/tmtysk/GestureDetector/src/com/appengine/hackathonjp/gesturedetector/service/GestureDetectorService.java
+++ trunk/Android20090319/SpecificDevice/tmtysk/GestureDetector/src/com/appengine/hackathonjp/gesturedetector/service/GestureDetectorService.java
@@ -1,45 +1,40 @@
 package com.appengine.hackathonjp.gesturedetector.service;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.os.RemoteException;
+import android.util.Log;
 
-/**
- * @author ogura
- * 
- * GestureDetectorService detects basic gesture
- * 
- * how to use:
- * 
- * GestureDetectorService.setGestureDetectorListener(new GestureDetectorListener() {
- *   public onBasicGestureDetect() {
- *   }
- * });
- * 
- * see below:
- * http://developerlife.com/tutorials/?p=356
- *
- */
 public class GestureDetectorService extends Service {
-	
-	private static List<GestureDetectorListener> gestureDetectorListeners;
-	
-	static {
-		gestureDetectorListeners = new ArrayList<GestureDetectorListener>();
-	}
-	
-	public static void setGestureDetectorListener(GestureDetectorListener l) {
-		gestureDetectorListeners.add(l);
-	}
 
 	@Override
 	public IBinder onBind(Intent intent) {
 		// TODO Auto-generated method stub
+		if (IGestureDetectorService.class.getName().equals(intent.getAction())) {
+			return mGesture;
+		}
 		return null;
 	}
 	
+	private final IGestureDetectorService.Stub mGesture = new IGestureDetectorService.Stub() {
+
+		@Override
+		public void registerGestureDetection(String detectionType,
+				IGestureDetectorListener listener) throws RemoteException {
+			// TODO Auto-generated method stub
+			Log.d("Service", "register");
+			listener.onBasicGestureDetect("");
+		}
+
+		@Override
+		public void unregisterGestureDetection(String detectionType,
+				IGestureDetectorListener listener) throws RemoteException {
+			// TODO Auto-generated method stub
+			
+		}
+
+		
+	};
 
 }
