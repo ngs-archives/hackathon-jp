a9d8e142524d700839c065c68db9db55bf99c763
ogurash <ogurash@bce7ee96-cdb0-11dd-aa7b-9fdd872f0417>
2009-03-19 06:46:04 +0000

    update service

--- trunk/Android20090319/SpecificDevice/tmtysk/GestureDetector/src/com/appengine/hackathonjp/gesturedetector/service/GestureDetectorService.java
+++ trunk/Android20090319/SpecificDevice/tmtysk/GestureDetector/src/com/appengine/hackathonjp/gesturedetector/service/GestureDetectorService.java
@@ -1,5 +1,10 @@
 package com.appengine.hackathonjp.gesturedetector.service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
@@ -7,6 +12,27 @@ import android.os.RemoteException;
 import android.util.Log;
 
 public class GestureDetectorService extends Service {
+	
+	Map<String, List<IGestureDetectorListener>> mListeners;
+	
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		
+		this.mListeners = new HashMap<String, List<IGestureDetectorListener>>();
+	}
+
+	@Override
+	public void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+	}
+
+	@Override
+	public void onStart(Intent intent, int startId) {
+		super.onStart(intent, startId);
+		
+	}
 
 	@Override
 	public IBinder onBind(Intent intent) {
@@ -22,16 +48,22 @@ public class GestureDetectorService extends Service {
 		@Override
 		public void registerGestureDetection(String detectionType,
 				IGestureDetectorListener listener) throws RemoteException {
-			// TODO Auto-generated method stub
-			Log.d("Service", "register");
-			listener.onBasicGestureDetect("");
+			Log.d("GestureDetectorService", "register: " + detectionType);
+			if (!mListeners.containsKey(detectionType) && mListeners.get(detectionType) == null) {
+				mListeners.put(detectionType, new ArrayList<IGestureDetectorListener>());
+			}
+			mListeners.get(detectionType).add(listener);
 		}
 
 		@Override
 		public void unregisterGestureDetection(String detectionType,
 				IGestureDetectorListener listener) throws RemoteException {
-			// TODO Auto-generated method stub
-			
+			if (mListeners.containsKey(detectionType) && mListeners.get(detectionType) != null) {
+				for (IGestureDetectorListener l : mListeners.get(detectionType)) {
+					if (l == listener)
+						mListeners.get(detectionType).remove(listener);
+				}
+			}
 		}
 
 		
