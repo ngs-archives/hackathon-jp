c4a78a47b18c03457c168530c126d9003811ff8a
vse5000 <vse5000@bce7ee96-cdb0-11dd-aa7b-9fdd872f0417>
2009-03-23 05:21:07 +0000

    Google Translate Apiと41ヶ国語翻訳版です。

--- Android20090320/Lifestyle/src/com/android/lifestyle/VoiceOfAndroid.java
+++ Android20090320/Lifestyle/src/com/android/lifestyle/VoiceOfAndroid.java
@@ -1,98 +1,79 @@
 package com.android.lifestyle;
 
+import com.google.api.translate.Language;
+
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.os.Bundle;
-import android.view.ContextMenu;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
 import android.view.View;
-import android.view.ContextMenu.ContextMenuInfo;
-import android.view.View.OnCreateContextMenuListener;
-import android.webkit.WebView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class VoiceOfAndroid extends Activity {
-	public String tmpText = "";
-
 
 	/** Called when the activity is first created. */
     @Override
+    /***
+     * This is OnCreate Override method
+     */
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        final AlertDialog.Builder ad = new AlertDialog.Builder(this).setMessage("test");
         final EditText text = (EditText)findViewById(R.id.text);
         registerForContextMenu(findViewById(R.id.text));
+        //Create From Spinner
+        Spinner fromSpinner = (Spinner) findViewById(R.id.fromSpinner);
+        ArrayAdapter<String> adapterFrom = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, SpinnerStrings);
+        adapterFrom.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        fromSpinner.setAdapter(adapterFrom);
 
-        // 日本語ボタン取得
-        final Button jaBtn =(Button)findViewById(R.id.jsave);
+        //Create To Spinner
+        Spinner toSpinner = (Spinner) findViewById(R.id.toSpinner);
+        ArrayAdapter<String> adapterTo = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, SpinnerStrings);
+        adapterTo.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        toSpinner.setAdapter(adapterTo);
+        
+        //Translation Button Event
+        final Button jaBtn =(Button)findViewById(R.id.transBtn);
         jaBtn.setOnClickListener(new Button.OnClickListener(){
 			public void onClick(View v) {
-				changeText("ej",text.getText().toString());
+				try {
+					Spinner fromSpinner = (Spinner) findViewById(R.id.fromSpinner);
+					String fromKey = SpinnerKeyStrings[fromSpinner.getSelectedItemPosition()];
+					Spinner toSpinner = (Spinner) findViewById(R.id.toSpinner);
+					String toKey = SpinnerKeyStrings[toSpinner.getSelectedItemPosition()];
+					String transResult = Translation.translate
+										(text.getText().toString(), fromKey, toKey);
+					TextView res = (TextView)findViewById(R.id.res);
+					res.setText(transResult);
+				} catch (Exception e){
+					Log.d("OnClick", e.getStackTrace().toString());
+				}
 			}
         });
-
-        // 英語ボタン取得
-        final Button enBtn =(Button)findViewById(R.id.esave);
-        enBtn.setOnClickListener(new Button.OnClickListener(){
-			public void onClick(View v) {
-				changeText("je",text.getText().toString());
-			}
-        });
-
-//        // フランス語ボタン取得
-//        final Button frBtn =(Button)findViewById(R.id.fsave);
-//        frBtn.setOnClickListener(new Button.OnClickListener(){
-//			public void onClick(View v) {
-//				changeText("fr",text.getText().toString());
-//			}
-//        });
     }
-
-    // 翻訳クラス処理に移動
-    public void changeText(String code, String text) {
-//    	final TextView word = (TextView)findViewById(R.id.text);
-
-    	Translation translation = new Translation();
-
-    	WebView view = new WebView(this);
-
-    	translation.work(view ,text, code);
-
-    	setContentView(view);
-
-
-
-//    	word.setText(text);
-    }
-
-	@Override
-	public void onCreateContextMenu(ContextMenu menu, View v,
-			ContextMenuInfo menuInfo) {
-		menu.add(0,0,Menu.NONE,"Japanese");
-		menu.add(0,1,Menu.NONE,"English");
-		menu.add(0,2,Menu.NONE,"Return");
-		super.onCreateContextMenu(menu, v, menuInfo);
-	}
-
-	@Override
-	public boolean onContextItemSelected(MenuItem item) {
-		final EditText text = (EditText)findViewById(R.id.text);
-
-		switch(item.getItemId()) {
-		case 0:
-			changeText("jp",text.getText().toString());
-			break;
-		case 1:
-			changeText("eng",text.getText().toString());
-			break;
-		}
-		return true;
-	}
-
-
+    /**
+     * Spinner Key array
+     */
+    private static final String[] SpinnerKeyStrings = (String[])Language.validLanguages.toArray();
+    /**
+     * Spinner Value array
+     */
+    private static final String[] SpinnerStrings = {
+        "", "ARABIC", "BULGARIAN", "CATALAN",
+        "CHINESE", "CHINESE_SIMPLIFIED", "CHINESE_TRADITIONAL",
+        "CROATIAN", "CZECH", "DANISH", "DUTCH",
+        "ENGLISH", "FILIPINO", "FINNISH", "FRENCH", "GALACIAN",
+        "GERMAN", "GREEK", "HEBREW", "HINDI", "HUNGARIAN",
+        "INDONESIAN", "ITALIAN", "JAPANESE", "KOREAN", "LATVIAN",
+        "LITHUANIAN", "MALTESE", "NORWEGIAN", "POLISH", "PORTUGESE",
+        "ROMANIAN", "RUSSIAN", "SERBIAN", "SLOVAK", "SLOVENIAN",
+        "SPANISH", "SWEDISH", "THAI", "TURKISH", "UKRANIAN",
+        "VIETNAMESE"
+        };
 }
