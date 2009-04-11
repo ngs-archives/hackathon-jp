16da3c2077827e6235db32471cb5cc16b8514ba8
sugi1982 <sugi1982@bce7ee96-cdb0-11dd-aa7b-9fdd872f0417>
2009-04-11 08:09:01 +0000

    post.py 整形

--- trunk/gae20090411/GData/src/post.py
+++ trunk/gae20090411/GData/src/post.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import time
 import datetime
 import StringIO
@@ -9,38 +10,30 @@ from dbclasses import *
 
 class PostHandler(webapp.RequestHandler):
     def post(self):
-        sio = StringIO.StringIO(self.request.get("image"))
-        tags = EXIF.process_file(sio)
-#         self.response.out.write("""<html>
-# <body>
-#   <table>
-# """)
-#         for key in tags.keys():
-#             if key in ('EXIF DateTimeOriginal', 'Image DateTime', 'EXIF DateTimeDigitized'):
-#                 self.response.out.write("    <tr><td>%s</td><td>%s</td></tr>\n" % (key, tags[key]))
-#             if key in ('GPS GPSLatitude', 'GPS GPSLongitude'):
-#                 deg = self.__getDegree(tags[key].values)
-#                 self.response.out.write("    <tr><td>%s</td><td>%f</td></tr>\n" % (key, deg))
-            
-#         self.response.out.write("""  </table>
-# </body>
-# </html>""")
-        
+        # 画像データの取得
         image = self.request.get("image")
+        # EXIFの解析
+        sio = StringIO.StringIO(image)
+        tags = EXIF.process_file(sio)
+        
+        # 画像の日付
         if tags.get('Image DateTime') != None:
             st = time.strptime(tags['Image DateTime'].values, '%Y:%m:%d %H:%M:%S')
-            imgDateTime =  datetime.datetime(st[0], st[1], st[2], st[3], st[4], st[5])
+            imgDateTime = datetime.datetime(*st[0:6])
         else:
             imgDateTime = None
+        # 緯度
         if tags.get('GPS GPSLatitude') != None:
             lat = self.__getDegree(tags['GPS GPSLatitude'].values)
         else:
             lat = 0.0
+        # 軽度
         if tags.get('GPS GPSLongitude') != None:
             lon = self.__getDegree(tags['GPS GPSLongitude'].values)
         else:
             lon = 0.0
 
+        # データの保存
         pictData = PictureData(picture     = image,
                                geo         = "%f, %f" % (lat, lon),
                                imgdatetime = imgDateTime)
@@ -48,6 +41,7 @@ class PostHandler(webapp.RequestHandler):
 
         self.redirect('/')
 
+    # EXIFの緯度経度情報からfloat型の数値への変換
     def __getDegree(self, values):
         deg  = values[0].num / values[0].den / 1.0
         deg += values[1].num / values[1].den / 60.0
