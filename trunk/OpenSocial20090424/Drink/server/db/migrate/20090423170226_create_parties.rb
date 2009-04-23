77c1417f8aa411245f43b52d90d8ea2a26cdd00a
tkyowa <tkyowa@bce7ee96-cdb0-11dd-aa7b-9fdd872f0417>
2009-04-23 17:23:29 +0000

    とりあえずモデルを置いておく

---
+++ trunk/OpenSocial20090424/Drink/server/db/migrate/20090423170226_create_parties.rb
@@ -0,0 +1,16 @@
+class CreateParties < ActiveRecord::Migration
+  def self.up
+    create_table :parties do |t|
+      t.column :name, :string
+      t.column :max, :integer
+      t.column :place, :string
+      t.column :description, :text
+      t.column :start_date, :datetime
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :parties
+  end
+end
