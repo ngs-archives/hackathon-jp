8819d7d5f8bee000a8d8b11af58a783d8adc00b6
tkyowa <tkyowa@bce7ee96-cdb0-11dd-aa7b-9fdd872f0417>
2009-04-24 04:27:28 +0000

    参加者の作成、一覧APIの実装

--- trunk/OpenSocial20090424/Drink/server/app/models/attender.rb
+++ trunk/OpenSocial20090424/Drink/server/app/models/attender.rb
@@ -1,3 +1,14 @@
 class Attender < ActiveRecord::Base
   belongs_to :party
+
+  def gender
+    case (id = read_attribute(:gender))
+    when 1
+      "男性"
+    when 2
+      "女性"
+    else
+      id
+    end
+  end
 end
