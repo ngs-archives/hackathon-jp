cff6f5f9af9067b6f9f9206d7560eec16a0215b1
hinaiji@po.sakura.ne.jp <hinaiji@po.sakura.ne.jp@bce7ee96-cdb0-11dd-aa7b-9fdd872f0417>
2009-05-15 07:15:32 +0000

    イベント登録追加、タブ切り替え

--- OpenSocial20090515/EventCalendar/canvas.js
+++ OpenSocial20090515/EventCalendar/canvas.js
@@ -8,7 +8,10 @@ function init() {
     
 	var tabSet = new gadgets.TabSet();
 	
-	tabSet.addTab("List");
+	tabSet.addTab("イベントを確認する", { contentContainer: document.getElementById("view") } );
+	tabSet.addTab("新しいイベントを登録する", { contentContainer: document.getElementById("register") } );
+	
+	_set_register_form();
 	
     var req = opensocial.newDataRequest();
     req.add(req.newFetchPersonRequest(
@@ -17,6 +20,27 @@ function init() {
     req.send(handleRequestMe);
 }
 
+function _set_register_form() {
+
+	var tbl = "";
+	
+	  tbl += "<form>";
+	  tbl += "タイトル<br /><input id=\"register_title\" type=\"text\" name=\"title\" /><br />";
+	  tbl += "コメント<br /><textarea id=\"register_comment\"  name=\"comment\"></textarea><br />";
+	  tbl += "<input id=\"register_submit\" type=\"button\" name=\"submit\" value=\"投稿 />";
+	  tbl += "</form>";
+
+	  $("#register").append(tbl);
+
+	  $("#register_submit").click( function() {
+		$("#register_content").value;
+		$("#register_comment").value;
+		
+		//createEntry
+		//addEntry
+		
+	  });
+}
 function handleRequestMe(data) {
 	  var owner = data.get("owner");
 	  if (owner.hadError()) {
@@ -27,58 +51,61 @@ function handleRequestMe(data) {
 	  var person = owner.getData();
 	  var id = person.getId();
 	  
-	  var events = ec_data.dao.getEventsByOwerUserId(id);
+	  var events = evtCal.dao.getEventsByOwerUserId(id);
 	  
 	  //No error. Do something with viewer.getData()...
 	  
 	  //table 
 	  var tbl = "";
 		  
-	  tbl += "<table border=\"1\"><tbody>";
-	  tbl += "<tr>";
-	  tbl += "<td>id</td>";
-	  tbl += "<td>title</td>";
-	  tbl += "<td>creator</td>";
-	  tbl += "<td>participants_count</td>";
-	  tbl += "<td>status</td>";
-	  tbl += "<td>entries_count</td>";
-	  tbl += "</tr>";
+	  tbl += "<table class=\"tbl_event\">";
+	  tbl += "<thead><tr>";
+	  tbl += "<td>件名</td>";
+	  tbl += "<td>作成者</td>";
+	  tbl += "<td>現在の参加者数</td>";
+	  tbl += "<td>状態</td>";
+	  tbl += "<td>投稿数</td>";
+	  tbl += "</tr></thead>";
 		          
+	  tbl += "<tbody>";
+	  
 	  jQuery.each(events, function(idx, event) {
 		  tbl += "<tr>";
-		  tbl += "<td><a href=\"javascript:view_detail(" + event.id + ");\">" + event.id + "</a></td>";
-		  tbl += "<td>" + event.title + "</td>";
+		  tbl += "<td><a href=\"javascript:view_detail(" + event.id + ");\">" + event.title + "</a></td>";
 		  tbl += "<td>" + event.creator.nickname + "</td>";
 		  tbl += "<td>" + event.participants.length + "</td>";
-		  tbl += "<td>" + ec_data.status.CREATING + "</td>";
+		  tbl += "<td>" + evtCal.status.CREATING + "</td>";
 		  tbl += "<td>" + event.entries.length + "</td>";
 		  tbl += "</tr>";
 	  });
 
 	  tbl += "</tbody></table>";
-	  
-	  $("#main").append(tbl);
+
+	  tbl += "<div id=\"detail\"></div>";
+
+	  $("#view").append(tbl);
 }
 
 function view_detail(event_id) {
 
-	  var event = ec_data.dao.getEventById(event_id);
+	  var event = evtCal.dao.getEventById(event_id);
 	  
 	  //table 
 	  var tbl = "";
 		  
-	  tbl += "<table border=\"1\"><tbody>";
+	  tbl += "<table class=\"tbl_event\"><tbody>";
 		          
-	  tbl += "<tr><td>id</td><td>" + event.id + "</td></tr>";
-	  tbl += "<tr><td>title</td><td>" + event.title + "</td></tr>";
-	  tbl += "<tr><td>creater</td><td>" + event.creator.nickname + "</td></tr>";
-	  tbl += "<tr><td>participants</td><td>" + event.participants.length + "</td></tr>";
-	  tbl += "<tr><td>status</td><td>" + ec_data.status.CREATING + "</td></tr>";
+	  tbl += "<tr><td>件名</td><td>" + event.title + "</td></tr>";
+	  tbl += "<tr><td>作成者</td><td>" + event.creator.nickname + "</td></tr>";
+	  tbl += "<tr><td>参加者</td><td>" + event.participants.join("、") + "</td></tr>";
+	  tbl += "<tr><td>状態</td><td>" + evtCal.status.CREATING + "</td></tr>";
 	  
 	  tbl += "</tbody></table>";
 
-	  tbl += "<table border=\"1\"><tbody>";
+	  tbl += "<table class=\"tbl_event\"><tbody>";
 
+	  tbl += "<tr><td>投稿:" + event.entries.length + "件</td></tr>";
+	  
 	  jQuery.each(event.entries, function(idx, entry) {
 		  tbl += "<tr>";
 		  tbl += "<td>" + entry.type + "</td>";
@@ -93,9 +120,30 @@ function view_detail(event_id) {
 	  });
 
 	  tbl += "</tbody></table>";
-	  
+
+	  tbl += "<form>タイプ<br />";
+	  tbl += "<input class=\"detail_type\" type=\"radio\" name=\"type\" value=\"what\"/>何を";
+	  tbl += "<input class=\"detail_type\"   type=\"radio\" name=\"type\" value=\"when\"/>いつ";
+	  tbl += "<input class=\"detail_type\"   type=\"radio\" name=\"type\" value=\"where\"/>どこで<br />";
+	  tbl += "きめたこと<br /><input id=\"detail_content\" type=\"text\" name=\"content\" /><br />";
+	  tbl += "コメント<br /><textarea id=\"detail_comment\"  name=\"comment\"></textarea><br />";
+	  tbl += "<input id=\"detail_submit\" type=\"button\" name=\"submit\" value=\"投稿 />";
+	  tbl += "</form>";
+
 	  $("#detail").empty();
 	  $("#detail").append(tbl);
+
+	  $("#detail_submit").click( function() {
+		$(".detail_type").value;
+		$("#detail_content").value;
+		$("#detail_comment").value;
+		
+		//createEntry
+		//addEntry
+		
+	  });
+
+	  gadgets.window.adjustHeight();
 }
 
 // TODO: Write the code for Canvas view.
