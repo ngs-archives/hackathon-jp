ebf3c4896f6dd2812cbeaea09369ba0146ac43e2
bols-blue@lnc.jp <bols-blue@lnc.jp@bce7ee96-cdb0-11dd-aa7b-9fdd872f0417>
2009-06-08 04:55:35 +0000

    dxfloder commit

---
+++ trunk/Android20090610/OpenGL/suehiro/src/jp/lnc/MeshLoader/MeshLoader.java
@@ -0,0 +1,18 @@
+package jp.lnc.MeshLoader;
+
+import java.io.File;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MeshLoader extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        PanMesh mesh = DxfLoader.DxfMeshLoader(new File("/sdcard/test.dxf"));
+        mesh.printMesh();
+          
+    }
+}
---
+++ trunk/Android20090610/OpenGL/suehiro/src/jp/lnc/MeshLoader/PanMesh.java
@@ -0,0 +1,24 @@
+package jp.lnc.MeshLoader;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PanMesh {
+	List<PanPrigon> panPrigonList = new ArrayList();
+
+	public PanPrigon newPrigon() {
+		PanPrigon newIns = new PanPrigon();
+		panPrigonList.add(newIns);
+		return newIns;
+	}
+
+	public void printMesh() {
+		for(int i=0;i<panPrigonList.size();i++){
+			panPrigonList.get(i).printPrigon();
+		}
+		
+	}
+	
+	
+	
+}
