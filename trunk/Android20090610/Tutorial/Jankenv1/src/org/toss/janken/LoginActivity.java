a954e8b7aa7ff7e1d1ca074014ee286c6ca2358f
mizmit1222 <mizmit1222@bce7ee96-cdb0-11dd-aa7b-9fdd872f0417>
2009-06-10 07:00:13 +0000

    add login activity.

--- trunk/Android20090610/Tutorial/Jankenv1/src/org/toss/janken/LoginActivity.java
+++ trunk/Android20090610/Tutorial/Jankenv1/src/org/toss/janken/LoginActivity.java
@@ -1,7 +1,12 @@
 package org.toss.janken;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class LoginActivity extends Activity {
-
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.login);
+	}
 }
