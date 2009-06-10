f47617a74ae77f26d0186b79557fce6b5028fcd6
mizmit1222 <mizmit1222@bce7ee96-cdb0-11dd-aa7b-9fdd872f0417>
2009-06-10 05:48:55 +0000

    This is actually an activity.

---
+++ Android20090610/Tutorial/Jankenv1/src/org/toss/janken/GameView.java
@@ -0,0 +1,16 @@
+package org.toss.janken;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class GameView extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		Log.v("Janken", "GameView");
+	}
+
+}
