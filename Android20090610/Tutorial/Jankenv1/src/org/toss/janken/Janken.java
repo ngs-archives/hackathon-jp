5d994900e3b6af3a33a713874ff846cdf210cbfe
mizmit1222 <mizmit1222@bce7ee96-cdb0-11dd-aa7b-9fdd872f0417>
2009-06-10 05:49:27 +0000

    Send an intent when button01 is clicked.

--- Android20090610/Tutorial/Jankenv1/src/org/toss/janken/Janken.java
+++ Android20090610/Tutorial/Jankenv1/src/org/toss/janken/Janken.java
@@ -1,13 +1,27 @@
 package org.toss.janken;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class Janken extends Activity {
+	private OnClickListener mButton01Listener = new OnClickListener() {
+	    public void onClick(View v) {
+	    	Intent i = new Intent(Janken.this, GameView.class);
+	    	startActivity(i);
+	    }
+	};
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        Button button = (Button)findViewById(R.id.Button01);
+        button.setOnClickListener(mButton01Listener);
     }
+    
 }
