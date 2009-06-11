245763b4773fd0b5322185d88ebfb5148c6cdd39
robert.gravina <robert.gravina@bce7ee96-cdb0-11dd-aa7b-9fdd872f0417>
2009-06-11 02:41:53 +0000

    refactored a little to a library

--- OpenSocial20090611/rating/ratings.js
+++ OpenSocial20090611/rating/ratings.js
@@ -1,3 +1,15 @@
 /*
  * ratings.js - An OpenSocial ratings library
  */
+
+/* globals */
+var RATINGS = {};
+RATINGS.CONFIG = {
+
+};
+
+function ratings_get_movie(url, id, callback) {
+  var params = {};
+  params[gadgets.io.RequestParameters.CONTENT_TYPE] = gadgets.io.ContentType.JSON;
+  gadgets.io.makeRequest(url, callback, params);  
+}
