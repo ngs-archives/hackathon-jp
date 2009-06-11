7603ab0c402bab9e93ce1987a4939e09f4229972
robert.gravina <robert.gravina@bce7ee96-cdb0-11dd-aa7b-9fdd872f0417>
2009-06-11 02:35:26 +0000

    displays title

--- trunk/OpenSocial20090611/rating/canvas.js
+++ trunk/OpenSocial20090611/rating/canvas.js
@@ -1,2 +1,20 @@
+var RATINGS = {};
+RATINGS.CONFIG = {
+    'url': "http://rating.local/",
+};
+
 function init() {
+  get_movie("star_trek");
+}
+
+function get_movie(id) {
+  var url = RATINGS.CONFIG["url"] + "movie_" + id + ".json";
+  var params = {};
+  params[gadgets.io.RequestParameters.CONTENT_TYPE] = gadgets.io.ContentType.JSON;
+  gadgets.io.makeRequest(url, on_movie, params);  
+}
+
+function on_movie(data) {
+  movie = data.data;
+  document.getElementById('title').innerHTML = movie.title;  
 }
