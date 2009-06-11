72dac8a247f6290e3b9e5cf9148c68b55532a7c0
youthk2dag <youthk2dag@bce7ee96-cdb0-11dd-aa7b-9fdd872f0417>
2009-06-11 04:35:24 +0000

    表示系を追加

--- OpenSocial20090611/interview/canvas.js
+++ OpenSocial20090611/interview/canvas.js
@@ -1,30 +1,59 @@
-gadgets.util.registerOnLoadHandler(function(){
+key_name = "data";
+
+gadgets.util.registerOnLoadHandler(function() {
     var req = opensocial.newDataRequest();
-    
+
     req.add(req.newFetchPersonRequest(opensocial.IdSpec.PersonId.OWNER), 'owner');
     req.add(req.newFetchPersonRequest(opensocial.IdSpec.PersonId.VIEWER), 'viewer');
-    
+
+	// get data
+	var keys = [key_name];
+	var idspec_params = {};
+	idspec_params[opensocial.IdSpec.Field.USER_ID] = opensocial.IdSpec.PersonId.OWNER;
+	var idspec = opensocial.newIdSpec(idspec_params);
+	var esc_params ={};
+	esc_params[opensocial.DataRequest.DataRequestFields.ESCAPE_TYPE] = opensocial.EscapeType.NONE;
+	req.add(req.newFetchPersonAppDataRequest(idspec, keys, esc_params), key_name);
+
     var ajp = new AjaxPages();
-    req.send(function(data){
-        // check viewer is owner or friend or ...
-        var owner = data.get('owner').getData();
-        var viewer = data.get('viewer').getData();
-        var param = {
-            owner: owner,
-            viewer: viewer
-        };
+    req.send(function(res){
+        // check viewer is owner
+        var owner = res.get('owner').getData();
+        var viewer = res.get('viewer').getData();
+		var stored_data = res.get(key_name).getData();
+
+		console.dir(stored_data);
+
+		var user_obj = stored_data[owner.getId()]//;[key_name];//.replace("&#34;","\"");
+		var obj = gadgets.json.parse(user_obj[key_name]);
+
+		var param = {
+			owner: owner,
+			viewer: viewer,
+			data: obj
+		};
+
         if (owner.getId() == viewer.getId()) {
             ajp.load("/interviews/canvas_ownerIsViewer.html");
             var processor = ajp.getProcessor();
             $("#container").html(processor(param));
         }
-        else {
-            ajp.load("/interviews/canvas_ownerIsNotViewer.html");
-            var processor = ajp.getProcessor();
-            $("#container").html(processor(param));
-        }
-        gadgets.window.adjustHeight();
-    });
+		else {
+			var now = new Date();
+			var limit = obj["limit"];
+			if(now < limit){
+				ajp.load("/interviews/canvas_ownerIsNotViewerYokoku.html");
+				var processor = ajp.getProcessor();
+				$("#container").html(processor(param));
+			}
+			else{
+				ajp.load("/interviews/canvas_ownerIsNotViewer.html");
+				var processor = ajp.getProcessor();
+				$("#container").html(processor(param));
+			}
+		}
+		gadgets.window.adjustHeight();
+	});
 });
 function showMessage(msg){
     var centerX = window.innerWidth / 2;
@@ -53,428 +82,428 @@ function showMessage(msg){
 //function init(){
 //
 //    var req = opensocial.newDataRequest();
-//    
+//
 //    req.add(req.newFetchPersonRequest(opensocial.IdSpec.PersonId.OWNER), 'owner');
-//    
+//
 //    req.add(req.newFetchPersonRequest(opensocial.IdSpec.PersonId.VIEWER), 'viewer');
-//    
+//
 //    var fparams = {};
-//    
+//
 //    fparams[opensocial.IdSpec.Field.USER_ID] = opensocial.IdSpec.PersonId.OWNER;
-//    
+//
 //    fparams[opensocial.IdSpec.Field.GROUP_ID] = 'FRIENDS';
-//    
+//
 //    var fidSpec = opensocial.newIdSpec(fparams);
-//    
+//
 //    req.add(req.newFetchPeopleRequest(fidSpec), 'friends');
-//    
+//
 //    var params = {};
-//    
+//
 //    params[opensocial.IdSpec.Field.USER_ID] = opensocial.IdSpec.PersonId.OWNER;
-//    
+//
 //    var idSpec = opensocial.newIdSpec(params);
-//    
+//
 //    var keys = [thisTodoNumKey];
-//    
+//
 //    req.add(req.newFetchPersonAppDataRequest(idSpec, keys), 'stored');
-//    
+//
 //    // get this mounth todo nums
-//    
+//
 //    var obj = {};
-//    
+//
 //    req.send(function(data){
-//    
+//
 //        var stored = data.get('stored').getData();
-//        
+//
 //        for (var id in stored) {
-//        
+//
 //            obj = stored[id];
-//            
+//
 //        }
-//        
+//
 //        if (obj[thisTodoNumKey] != null) {
-//        
+//
 //            thisTodoNum = obj[thisTodoNumKey];
-//            
+//
 //        }
-//        
+//
 //        else {
-//        
+//
 //            thisTodoNum = 0;
-//            
+//
 //        }
-//        
+//
 //        // check viewer is owner or friend or ...
-//        
+//
 //        var owner = data.get('owner').getData();
-//        
+//
 //        var viewer = data.get('viewer').getData();
-//        
+//
 //        // get and show owner's nickname
-//        
+//
 //        var nameParams = {};
-//        
+//
 //        nameParams[opensocial.DataRequest.DataRequestFields.ESCAPE_TYPE] = opensocial.EscapeType.NONE;
-//        
+//
 //        var nickname = owner.getField(opensocial.Person.Field.NICKNAME, params);
-//        
+//
 //        document.getElementById('owner_name').innerHTML += 'these are ' + nickname + '\'s future.';
-//        
+//
 //        if (owner.getId() == viewer.getId()) {
-//        
+//
 //            hiddenHappyButton();
-//            
+//
 //        }
-//        
+//
 //        else {
-//        
+//
 //            var friends = data.get('friends').getData();
-//            
+//
 //            var isFriendFlag = false;
-//            
+//
 //            friends.each(function(friend){
-//            
+//
 //                if (friend.getId() == viewer.getId()) {
-//                
+//
 //                    isFriendFlag = true;
-//                    
+//
 //                }
-//                
+//
 //            });
-//            
+//
 //            if (isFriendFlag == false) {
-//            
+//
 //                hiddenHappyButton();
-//                
+//
 //            }
-//            
+//
 //        }
-//        
+//
 //        gadgets.window.adjustHeight();
-//        
+//
 //    });
-//    
+//
 //}
 //
 //function addTodo(){
 //
 //    var tm = thisMonth;
-//    
+//
 //    var ttn = thisTodoNum;
-//    
+//
 //    var req = opensocial.newDataRequest();
-//    
+//
 //    // add toto content
-//    
+//
 //    var content = document.getElementById('todo_content').value;
-//    
+//
 //    var appDataKey = "content" + thisMonth + "_" + ttn;
-//    
+//
 //    req.add(req.newUpdatePersonAppDataRequest(opensocial.IdSpec.PersonId.OWNER, appDataKey, content));
-//    
+//
 //    // add todo flag 0 or 1
-//    
+//
 //    var todoFlag = 0;
-//    
+//
 //    var todoFlagKey = "check" + thisMonth + "_" + ttn;
-//    
+//
 //    req.add(req.newUpdatePersonAppDataRequest(opensocial.IdSpec.PersonId.OWNER, todoFlagKey, todoFlag));
-//    
+//
 //    // add month4_num ++
-//    
+//
 //    if (thisTodoNum < maxTodoNum) {
-//    
+//
 //        var newTodoNum = Number(thisTodoNum) + 1;
-//        
+//
 //        req.add(req.newUpdatePersonAppDataRequest(opensocial.IdSpec.PersonId.OWNER, thisTodoNumKey, newTodoNum));
-//        
+//
 //    }
-//    
+//
 //    else {
-//    
+//
 //        alert("もう無理！");
-//        
+//
 //        return false;
-//        
+//
 //    }
-//    
+//
 //    req.send(function(response){
-//    
+//
 //        if (response.hadError()) {
-//        
+//
 //            document.getElementById('result_appdata').innerHTML = response.getErrorCode();
-//            
+//
 //        }
 //        else {
-//        
+//
 //            document.getElementById('result_appdata').innerHTML = '';
-//            
+//
 //            // show new todo
-//            
+//
 //            clearShowTodo();
-//            
+//
 //            showTodoList();
-//            
+//
 //        }
-//        
+//
 //        gadgets.window.adjustHeight();
-//        
+//
 //        init();
-//        
+//
 //    });
-//    
+//
 //}
 //
 //function clearShowTodo(){
 //
 //    var parent = document.getElementById('contents');
-//    
+//
 //    for (var i = parent.childNodes.length - 1; i >= 0; i--) {
-//    
+//
 //        parent.removeChild(parent.childNodes[i]);
-//        
+//
 //    }
-//    
+//
 //}
 //
 //function hiddenHappyButton(){
 //
 //    var fv = document.getElementById('friend_view');
-//    
+//
 //    fv.style.display = 'none';
-//    
+//
 //}
 //
 //function showTodoList(){
 //
 //    var tm = thisMonth;
-//    
+//
 //    var req = opensocial.newDataRequest();
-//    
+//
 //    req.add(req.newFetchPersonRequest(opensocial.IdSpec.PersonId.OWNER), 'owner');
-//    
+//
 //    req.add(req.newFetchPersonRequest(opensocial.IdSpec.PersonId.VIEWER), 'viewer');
-//    
+//
 //    var params = {};
-//    
+//
 //    params[opensocial.IdSpec.Field.USER_ID] = opensocial.IdSpec.PersonId.OWNER;
-//    
+//
 //    var idSpec = opensocial.newIdSpec(params);
-//    
+//
 //    var keys = [];
-//    
+//
 //    for (var i = 0; i < maxTodoNum; i++) {
-//    
+//
 //        keys[i * 2] = "check" + tm + "_" + i;
-//        
+//
 //        keys[i * 2 + 1] = "content" + tm + "_" + i;
-//        
+//
 //    }
-//    
+//
 //    req.add(req.newFetchPersonAppDataRequest(idSpec, keys), 'stored');
-//    
+//
 //    req.send(function(data){
-//    
+//
 //        var owner = data.get('owner').getData();
-//        
+//
 //        var viewer = data.get('viewer').getData();
-//        
+//
 //        var isOwnerFlag = false;
-//        
+//
 //        if (owner.getId() == viewer.getId()) {
-//        
+//
 //            isOwnerFlag = true;
-//            
+//
 //        }
-//        
+//
 //        // show stored data
-//        
+//
 //        var stored = data.get('stored').getData();
-//        
+//
 //        var isDataExist = false;
-//        
+//
 //        var isChecked = false;
-//        
+//
 //        for (var id in stored) {
-//        
+//
 //            var obj = stored[id];
-//            
+//
 //            for (var keyid in keys) {
-//            
+//
 //                if (obj[keys[keyid]] != null) {
-//                
+//
 //                    isDataExist = true;
-//                    
+//
 //                    if (isOwnerFlag == true) {
-//                    
+//
 //                        if (keyid % 2 == 0) {
-//                        
+//
 //                            if (obj[keys[keyid]] == 1) {
-//                            
+//
 //                                isChecked = true;
-//                                
+//
 //                            }
-//                            
+//
 //                            else {
-//                            
+//
 //                                isChecked = false;
-//                                
+//
 //                            }
-//                            
+//
 //                        }
-//                        
+//
 //                        else {
-//                        
+//
 //                            if (isChecked == true) {
-//                            
+//
 //                                document.getElementById('contents').innerHTML += '<li><input type="checkbox" onclick="checkTodo(' + String(keys[keyid]) + '); checked><span id=deco_' + keys[keyid] + '>' + obj[keys[keyid]] + '</span></li>';
-//                                
+//
 //                            }
-//                            
+//
 //                            else {
-//                            
+//
 //                                document.getElementById('contents').innerHTML += '<li><input type="checkbox" onclick="checkTodo(' + String(keys[keyid]) + ');><span id=deco_' + keys[keyid] + '>' + obj[keys[keyid]] + '</span></li>';
-//                                
+//
 //                            }
-//                            
+//
 //                        }
-//                        
+//
 //                    }
-//                    
+//
 //                    else {
-//                    
+//
 //                        if (keyid % 2 == 1) {
-//                        
+//
 //                            document.getElementById('contents').innerHTML += '<li><span id=deco_' + keys[keyid] + '>' + obj[keys[keyid]] + '</span></li>';
-//                            
+//
 //                        }
-//                        
+//
 //                    }
-//                    
+//
 //                }
-//                
+//
 //            }
-//            
+//
 //        }
-//        
+//
 //        if (isDataExist == false) {
-//        
+//
 //            document.getElementById('contents').innerHTML += '<li>今のままでは<br>' + nickname + 'は幸せになれない･･･。</li>';
-//            
+//
 //        }
-//        
+//
 //        gadgets.window.adjustHeight();
-//        
+//
 //    });
-//    
+//
 //}
 //
 //function checkTodo(todoId){
 //
 //    var tId = String(todoId);
-//    
+//
 //    if (thisMonth < 10) {
-//    
+//
 //        tId = tId.slice(13);
-//        
+//
 //    }
-//    
+//
 //    else {
-//    
+//
 //        tId = tId.slice(14);
-//        
+//
 //    }
-//    
+//
 //    var req = opensocial.newDataRequest();
-//    
+//
 //    var params = {};
-//    
+//
 //    params[opensocial.IdSpec.Field.USER_ID] = opensocial.IdSpec.PersonId.OWNER;
-//    
+//
 //    var idSpec = opensocial.newIdSpec(params);
-//    
+//
 //    var todoFlagKeyName = "check" + thisMonth + "_" + tId;
-//    
+//
 //    var todoContentKeyName = "deco_content" + thisMonth + "_" + tId;
-//    
+//
 //    var keys = [todoFlagKeyName];
-//    
+//
 //    req.add(req.newFetchPersonAppDataRequest(idSpec, keys), 'stored');
-//    
+//
 //    // get this mounth todo nums
-//    
+//
 //    var obj = {};
-//    
+//
 //    var targetTodoFlag = 0;
-//    
+//
 //    req.send(function(data){
-//    
+//
 //        var stored = data.get('stored').getData();
-//        
+//
 //        for (var id in stored) {
-//        
+//
 //            obj = stored[id];
-//            
+//
 //        }
-//        
+//
 //        targetTodoFlag = obj[keys];
-//        
+//
 //        if (targetTodoFlag == 0) {
-//        
+//
 //            saveTodoFlag(todoId, 1);
-//            
+//
 //            document.getElementById(todoContentKeyName).style.textDecoration = 'line-through';
-//            
+//
 //            document.getElementById(todoContentKeyName).style.color = '#aaaaaa';
-//            
+//
 //        }
-//        
+//
 //        else {
-//        
+//
 //            saveTodoFlag(todoId, 0);
-//            
+//
 //            document.getElementById(todoContentKeyName).style.textDecoration = 'none';
-//            
+//
 //            document.getElementById(todoContentKeyName).style.color = '#000000';
-//            
+//
 //        }
-//        
+//
 //        // 達成度の更新
-//    
+//
 //    });
-//    
+//
 //}
 //
 //function saveTodoFlag(todoId, value){
 //
 //    var tId = todoId;
-//    
+//
 //    var val = value;
-//    
+//
 //    var req = opensocial.newDataRequest();
-//    
+//
 //    // add todo flag 0 or 1
-//    
+//
 //    var todoFlagKey = "check" + thisMonth + "_" + tId;
-//    
+//
 //    req.add(req.newUpdatePersonAppDataRequest(opensocial.IdSpec.PersonId.OWNER, todoFlagKey, value));
-//    
+//
 //    req.send(function(response){
-//    
+//
 //        if (response.hadError()) {
-//        
+//
 //            document.getElementById('result_appdata').innerHTML = response.getErrorCode();
-//            
+//
 //        }
 //        else {
-//        
+//
 //            document.getElementById('result_appdata').innerHTML = '';
-//            
+//
 //        }
-//        
+//
 //        gadgets.window.adjustHeight();
-//        
+//
 //    });
-//    
+//
 //}
 //
--- OpenSocial20090611/interview/canvas_ownerIsNotViewerYokoku.js
+++ OpenSocial20090611/interview/canvas_ownerIsNotViewerYokoku.js
@@ -1,21 +1,19 @@
 var owner_name;
 var title;
-var data;
+var hint;
+var limit;
 
 
 function onOwnerIsNotViewer(param) {
 
 	var owner = param.owner;
 	var viewer = param.viewer;
-//	console.log("aaa" + owner);
 
 	owner_name = owner.getDisplayName();
 
 	title = param.data["title"];
-	data = param.data["data"];
-
-//	console.log("bbb" + title);
-
+	hint = param.data["hint"];
+	limit = param.data["limit"];
 
 	var req = opensocial.newDataRequest();
 	req.send(function(res) {
