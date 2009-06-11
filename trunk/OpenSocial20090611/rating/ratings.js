7a9a3289f76bd44d5d23edb7ab71be2a6bf80b98
robert.gravina <robert.gravina@bce7ee96-cdb0-11dd-aa7b-9fdd872f0417>
2009-06-11 09:54:55 +0000

    updates

--- trunk/OpenSocial20090611/rating/ratings.js
+++ trunk/OpenSocial20090611/rating/ratings.js
@@ -8,6 +8,9 @@ RATINGS.CONFIG = {
 
 };
 
+RATINGS.item = null;
+RATINGS.rating = null;
+
 /*
  gets json for one item 
 */
@@ -22,3 +25,26 @@ function ratings_get_rating(url, id, callback) {
   params[gadgets.io.RequestParameters.CONTENT_TYPE] = gadgets.io.ContentType.JSON;
   gadgets.io.makeRequest(url, callback, params);  
 }
+
+function ratings_rate(url, item, rating) {
+  /* call remote server */ 
+  var req = opensocial.newDataRequest();
+  var viewer = req.add(req.newFetchPersonRequest("VIEWER"), 'viewer');
+  RATINGS.item = item;
+  RATINGS.rating = rating;
+   req.send(on_get_viewer);
+}
+
+function on_get_viewer(data) {
+  var viewer = data.get('viewer').getData();
+  
+  /* create the message */
+  var message = "User " + viewer.getDisplayName() + " rated " + RATINGS.item.title + " as " + RATINGS.rating;
+
+  /* send to activity stream */
+  var params = {};
+  params[opensocial.Activity.Field.TITLE] = message;
+  var activity = opensocial.newActivity(params)
+  opensocial.requestCreateActivity(activity, opensocial.CreateActivityPriority.HIGH, function() {});  
+}
+
