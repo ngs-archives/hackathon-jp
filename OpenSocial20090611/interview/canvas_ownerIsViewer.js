2dc0fc4a15ec6f874fe47138a4d2f794bf3e6e57
shumpei.shiraishi <shumpei.shiraishi@bce7ee96-cdb0-11dd-aa7b-9fdd872f0417>
2009-06-11 03:37:36 +0000

    データ登録側完成

--- OpenSocial20090611/interview/canvas_ownerIsViewer.js
+++ OpenSocial20090611/interview/canvas_ownerIsViewer.js
@@ -1,5 +1,29 @@
-function onOwnerIsViewer(param) {
-	var owner = param.owner;
-	var viewer = param.viewer;
-	console.log("aaa" + owner);
+var DEFAULT_DATETIME_FORMAT = new DateFormat("yyyy/MM/dd HH:mm");
+
+function onOwnerIsViewer(param){
+    var owner = param.owner;
+    var viewer = param.viewer;
+}
+
+function registerMyAnnounce(){
+    var title = $("#title").val();
+    var hint = $("#hint").val();
+    var limit = $("#limit").val();
+    var data = $("#data").val();
+    var limitDate = DEFAULT_DATETIME_FORMAT.parse(limit);
+    
+    var req = opensocial.newDataRequest();
+    req.add(req.newUpdatePersonAppDataRequest("OWNER", "data", {
+        title: title,
+        hint: hint,
+        limit: limitDate.getTime(),
+        data: data
+    }), "response");
+    req.send(function(data){
+        if (data.hadError()) {
+            showMessage("エラーが発生しました。");
+            return;
+        }
+        alert("成功しました！");
+    });
 }
