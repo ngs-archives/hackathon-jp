9b1d72717b90587f26f11c49c1894fe12849388e
shumpei.shiraishi <shumpei.shiraishi@bce7ee96-cdb0-11dd-aa7b-9fdd872f0417>
2009-06-11 05:33:34 +0000

    予告篇ページを作った

--- trunk/OpenSocial20090611/interview/canvas_ownerIsNotViewerYokoku.js
+++ trunk/OpenSocial20090611/interview/canvas_ownerIsNotViewerYokoku.js
@@ -1,22 +1,34 @@
-var owner_name;
-var title;
-var hint;
-var limit;
+function onOwnerIsNotViewer_yokoku(param){
+    var owner = param.owner;
+    var viewer = param.viewer;
+    owner_name = owner.getDisplayName();
+    
+    var title = param.data["title"];
+    var hint = param.data["hint"];
+    var limit = param.data["limit"];
 
-
-function onOwnerIsNotViewer(param) {
-
-	var owner = param.owner;
-	var viewer = param.viewer;
-
-	owner_name = owner.getDisplayName();
-
-	title = param.data["title"];
-	hint = param.data["hint"];
-	limit = param.data["limit"];
-
-	var req = opensocial.newDataRequest();
-	req.send(function(res) {
-		gadgets.window.adjustHeight();
-	});
+	var timer = setInterval(function() {
+		var now = new Date().getTime();
+		var interval = limit - now;
+		if (interval <= 0) {
+			clearInterval(timer);
+			return;
+		}
+		var intervalSeconds = Math.floor(interval / 1000);
+		var msg = "<span class='seconds'>" + (intervalSeconds % 60) + "秒</span>";
+		if (intervalSeconds > 60) {
+			var intervalMinutes = Math.floor(intervalSeconds / 60);
+			msg = "<span class='minutes'>" + (intervalMinutes % 60) + "分</span>" + msg;
+		}
+		if (intervalMinutes > 60) {
+			var intervalHours = Math.floor(intervalMinutes / 60);
+			msg = "<span class='hours'>" + (intervalHours % 24) + "時間</span>" + msg;
+		}
+		if (intervalHours > 24) {
+			var intervalDays = Math.floor(intervalHours / 24);
+			msg = "<span class='days'>" + intervalDays + "日</span>" + msg;
+		}
+		$("#remainedTime").html(msg);
+	}, 300);
+	gadgets.window.adjustHeight();
 }
