a7ff866ad0b80c64333a16b036dd2556b1bc8ecd
robert.gravina <robert.gravina@bce7ee96-cdb0-11dd-aa7b-9fdd872f0417>
2009-06-11 03:22:51 +0000

    basic display of sample data

--- OpenSocial20090611/rating/canvas.js
+++ OpenSocial20090611/rating/canvas.js
@@ -1,14 +1,16 @@
 var WIDGET = {};
 WIDGET.CONFIG = {
+    'data_url': "http://rating.local/",
     'ratings_url': "http://rating.local/",
 };
 
 function init() {
   get_movie("star_trek");
+  get_ratings("star_trek");
 }
 
 function get_movie(id) {
-  var url = WIDGET.CONFIG["ratings_url"] + "movie_" + id + ".json";
+  var url = WIDGET.CONFIG["data_url"] + "movie_" + id + ".json";
   ratings_get_item(url, id, on_movie);
 }
 
@@ -16,4 +18,17 @@ function on_movie(data) {
   movie = data.data;
   document.getElementById('title').innerHTML = movie.title;
   console.log(movie);
+}
+
+
+
+function get_ratings(id) {
+  var url = WIDGET.CONFIG["ratings_url"] + "ratings_" + id + ".json";
+  ratings_get_rating(url, id, on_rating);
+}
+
+function on_rating(data) {
+  rating = data.data;
+  document.getElementById('average_rating').innerHTML = rating.average_rating;
+  document.getElementById('total_ratings').innerHTML = rating.total_ratings;
 }
