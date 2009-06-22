374be84cb505b2d66536c71ed2d6d82277393443
wada@jugem.co.jp <wada@jugem.co.jp@bce7ee96-cdb0-11dd-aa7b-9fdd872f0417>
2009-06-22 04:21:40 +0000

    config.jsに定義情報のみ外出し

---
+++ trunk/OpenSocial20090611/moso/config.js
@@ -0,0 +1,5 @@
+RESOURCE_URL_ROOT = 'http://files.getdropbox.com/u/536450/moso';
+
+HOME_HTML_URL = RESOURCE_URL_ROOT + '/home.html';
+PROFILE_HTML_URL = RESOURCE_URL_ROOT + '/profile.html';
+CANVAS_HTML_URL = RESOURCE_URL_ROOT + '/canvas.html';
--- trunk/OpenSocial20090611/moso/moso.js
+++ trunk/OpenSocial20090611/moso/moso.js
@@ -1,9 +1,3 @@
-RESOURCE_URL_ROOT = 'http://files.getdropbox.com/u/536450/moso';
-
-HOME_HTML_URL = RESOURCE_URL_ROOT + '/home.html';
-PROFILE_HTML_URL = RESOURCE_URL_ROOT + '/profile.html';
-CANVAS_HTML_URL = RESOURCE_URL_ROOT + '/canvas.html';
-
 //外部ホストへフォームデータを送信する
 
 $(function(){
