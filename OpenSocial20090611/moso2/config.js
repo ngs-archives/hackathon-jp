c60afbb25acb876a232ddfe5432c0a845406a090
chii8n <chii8n@bce7ee96-cdb0-11dd-aa7b-9fdd872f0417>
2009-07-17 05:26:52 +0000

    new moso directory

---
+++ OpenSocial20090611/moso2/config.js
@@ -0,0 +1,6 @@
+HOST_URL = 'http://ec2-174-129-93-227.compute-1.amazonaws.com';
+RESOURCE_URL_ROOT = 'http://chii8n.googlecode.com/svn/trunk/moso';
+
+HOME_HTML_URL = RESOURCE_URL_ROOT + '/home.html';
+PROFILE_HTML_URL = RESOURCE_URL_ROOT + '/profile.html';
+CANVAS_HTML_URL = RESOURCE_URL_ROOT + '/canvas.html';
