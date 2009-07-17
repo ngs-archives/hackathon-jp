9f8fc58a0c8c61a5f57b7763885a7c01b2c8cac9
yshida <yshida@bce7ee96-cdb0-11dd-aa7b-9fdd872f0417>
2009-07-17 05:48:04 +0000

    waiting roomの部分が一応できた

--- trunk/OpenSocial20090717/diamond/gadget/shida.js
+++ trunk/OpenSocial20090717/diamond/gadget/shida.js
@@ -27,7 +27,9 @@ diamond.show_room_list = function() {
 
 diamond.join = function(owner_id) {
     
+    diamond.owner_id = owner_id;
     cods.fetchViewerProfile(function(viewer) {
+        diamond.viewer = viewer;
 	console.log(viewer);
 	var viewer_id     = viewer.getId();
 	var nickname      = viewer.getDisplayName();
@@ -42,14 +44,49 @@ diamond.join = function(owner_id) {
 	cods.makeRequest(url,
 			 null,
 			 function(game_status) {
-			     diamond.show_room(game_status);
+			     diamond.show_waiting_room(game_status);
 			 },
 			 function() {
 			 });
     });
 }
 
-diamond.show_room = function(game_status) {
-    
+diamond.show_waiting_room = function(game_status) {
+    $('#room_list').hide();
+    $('#waiting_room').show();
+    console.log(game_status);
 
+    if (game_status.gameStatus == 'waiting_answer') {
+        return diamond.play_game(game_status);
+    }
+
+    waiting_room_html = '<ul>';
+    $.each(game_status.players, function(index, player) {
+	waiting_room_html += 
+	    '<li>' + 
+	    '<img  class="thumbnail_url" src="' + player.thumbUrl + '" />' + 
+            '<span class="nickname">'           + player.displayName + '</span>' + 
+	    '</li>';
+    });
+
+    $('#waiting_room').html(waiting_room_html);
+
+    setTimeout(diamond.refresh_waiting_room, 1000);
 }
+
+diamond.refresh_waiting_room = function() {
+    console.log('refresh_waiting_room');
+    var owner_id  = diamond.owner_id;
+    var viewer_id = diamond.viewer.getId();
+    var url = 'http://codess.heteml.jp/diamond/server/?' + 
+        'owner_id=' + owner_id  + '&' + 
+        'user_id='  + viewer_id + '&' + 
+        'action=';
+    cods.makeRequest(url,
+                     null,
+                     function(game_status) {
+                         diamond.show_waiting_room(game_status);
+                     },
+                     function() {
+                     });
+};
