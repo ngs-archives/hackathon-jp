d9662ce356c0022b4ddef7f014f65d7e7c38d2f4
ku0522a@gmail.com <ku0522a@gmail.com@bce7ee96-cdb0-11dd-aa7b-9fdd872f0417>
2009-11-21 04:43:51 +0000

    add pageContent -> contentScript communication.

--- trunk/ChromeExt20091121/html5/scrapbook/scrapbook.js
+++ trunk/ChromeExt20091121/html5/scrapbook/scrapbook.js
@@ -1,14 +1,23 @@
 
+function initEventListener () {
+	document.addEventListener('ScrapBook.downloadComplete', function (ev) {
+		notifyDownloadCompleted(
+			document.location.href,
+			document.title
+		);
+	}, false);
+}
+
+function init () {
 	var connection = chrome.extension.connect();
+	initEventListener();
+}
 function notifyDownloadCompleted(url, title) {
 	connection.postMessage( {
 		command: "onDownloadComplete",
 		url: url, title: title
 	} );
 }
-/* for test
-notifyDownloadCompleted(
-		document.location.href,
-		document.title
-);
-*/
+
+init();
+
