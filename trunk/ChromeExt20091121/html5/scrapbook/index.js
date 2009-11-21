4ca6ba4c5e617e6d053e07a8d4191aa68ffacd4e
miyamototakuya <miyamototakuya@bce7ee96-cdb0-11dd-aa7b-9fdd872f0417>
2009-11-21 05:14:16 +0000

    view未テスト

---
+++ trunk/ChromeExt20091121/html5/scrapbook/index.js
@@ -0,0 +1,31 @@
+
+// cashInfo: {
+	//	"http://yahoo.com/index.html":{
+//		title:"yahoo",
+//		savedDateTime:"Nov 20 2009",
+//		url:"test"
+//	}
+//}
+var mainListId = "#mainList";
+
+window.onload = function(){
+	//alert('hello world');
+	
+	var mainList = $(mainListId);
+	var cachedInfo = window.localStorage.cachedInfo;
+
+	for(var pageUrl in cachedInfo){
+		var cachedInfoElement = cachedInfo[pageUrl];
+		$('<li />')
+		.append(
+			$('<a/>')
+			.attr('href', cachedInfoElement.url)
+			.text(cachedInfoElement.title))
+		.appendTo(mainList);
+	}
+	/*
+	for(var i = 0; i < 10; i++){
+		$('<li />').append($('<a/>').attr('href', 'test').text('test')).appendTo(mainList);
+	}
+	*/
+};
