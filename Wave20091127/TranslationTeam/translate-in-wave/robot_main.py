4b88ac6cc9c7902892a9b7e08425fc05f6e52141
matsuo.takashi <matsuo.takashi@bce7ee96-cdb0-11dd-aa7b-9fdd872f0417>
2009-11-27 05:44:27 +0000

    added catalog app

--- Wave20091127/TranslationTeam/translate-in-wave/robot_main.py
+++ Wave20091127/TranslationTeam/translate-in-wave/robot_main.py
@@ -21,6 +21,24 @@ gadget_url = ('http://hackathon-jp.googlecode.com/svn/trunk/'
               'Wave20091127/TranslationTeam/gadget/'
               'translation-helper.xml')
 
+KEY_REQUEST_TYPE = "requestType"
+KEY_RESPONSE_TYPE = "responseType"
+KEY_PATH = "path"
+KEY_LANGUAGE = "language"
+KEY_STATUS = "status"
+KEY_DATA = "data"
+
+REQUEST_PO = "REQUEST_PO"
+RESPONSE_PO = "RESPONSE_PO"
+
+STATUS_PO_EXIST = "200"
+STATUS_PO_NOT_EXIST = "null.po"
+
+MOCK_PO_DATA = """
+"""
+
+from catalog.models import PoFile
+
 def on_self_added(properties, context):
   """Called when this robot is first added to the wave."""
   blip = context.GetBlipById(context.GetRootWavelet().GetRootBlipId())
@@ -29,6 +47,20 @@ def on_self_added(properties, context):
     blip.GetDocument().AppendElement(
       document.Gadget(gadget_url))
 
+def handle_request_po(gadget):
+  path = gadget.get(KEY_PATH)
+  language = gadget.get(KEY_LANGUAGE)
+  # po_file = PoFile.all().filter("path =", path)\
+  #    .filter("language =", language)
+  ret = {
+    KEY_RESPONSE_TYPE: RESPONSE_PO,
+    KEY_STATUS: STATUS_PO_EXIST,
+    KEY_DATA: MOCK_PO_DATA,
+  }
+
+handler_map = {
+  REQUEST_PO: handle_request_po,
+}
 
 def OnBlipSubmitted(properties, context):
   logging.info('OnBlipSubmitted')
@@ -36,14 +68,18 @@ def OnBlipSubmitted(properties, context):
   blip = context.GetBlipById(blip_id)
   doc = blip.GetDocument()
   gadget = blip.GetGadgetByUrl(gadget_url)
-  hoge = gadget.get("hoge")
-  logging.debug("hoge: %s" % hoge)
-  delta = {"hoge": "fuga"}
-  doc.GadgetSubmitDelta(gadget, delta)
+  request_type = gadget.get(KEY_REQUEST_TYPE, REQUEST_PO)
+  method = handler_map.get(request_type)
+  assert(callable(method))
+  method(gadget)
+#   hoge = gadget.get("hoge")
+#   logging.debug("hoge: %s" % hoge)
+#   delta = {"hoge": hoge+"fuga"}
+#   doc.GadgetSubmitDelta(gadget, delta)
 
 if __name__ == '__main__':
   logging.getLogger().setLevel(logging.DEBUG)
-  r = robot.Robot('Translation Helper', '1.0',
+  r = robot.Robot('Translation Helper', '2.0',
                   image_url='http://%s/media/icon.png' % hostname,
                   profile_url='http://%s/_wave/robot/profile' % hostname)
   r.RegisterHandler(events.WAVELET_SELF_ADDED,
