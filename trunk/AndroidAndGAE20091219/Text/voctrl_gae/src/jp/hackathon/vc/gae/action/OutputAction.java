5ca329db06f86b26b3d9674818843227f27ccfb2
miyamototakuya@gmail.com <miyamototakuya@gmail.com@bce7ee96-cdb0-11dd-aa7b-9fdd872f0417>
2009-12-19 07:56:06 +0000

    add delete function

--- trunk/AndroidAndGAE20091219/Text/voctrl_gae/src/jp/hackathon/vc/gae/action/OutputAction.java
+++ trunk/AndroidAndGAE20091219/Text/voctrl_gae/src/jp/hackathon/vc/gae/action/OutputAction.java
@@ -1,9 +1,11 @@
 package jp.hackathon.vc.gae.action;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.List;
 
 import javax.jdo.PersistenceManager;
@@ -16,23 +18,18 @@ public class OutputAction implements IAction {
 
 	@Override
 	public void run(HttpServletRequest req, HttpServletResponse resp, Object data) throws IOException {
-		// resp.getWriter().println("output test");
-
 		PersistenceManager pm = PMF.get().getPersistenceManager();
-
+//		GoogleQuery gq = new GoogleQuery();
 		try {
-
 			String query = "select from " + VoctrlModel.class.getName();
 			List<VoctrlModel> models = (List<VoctrlModel>) pm.newQuery(query).execute();
-
-			//resp.setContentType("text/plain");
 			
 			PrintWriter writer = resp.getWriter();
-
 			for (VoctrlModel voctrlModel : models) {
 				writer.println(voctrlModel.getData());
+//				gq.makeQuery(query, writer);
 			}
-//			pm.deletePersistentAll(models);
+			pm.deletePersistentAll(models);
 		} catch (Exception e) {
 			resp.getWriter().append("ERROR");
 			e.printStackTrace();
@@ -41,5 +38,28 @@ public class OutputAction implements IAction {
 		}
 
 	}
+	
+//	private void search(String query){
+//		String key = "ABQIAAAAMI2qOraKf1AKK762okOfpxRdIXpbsmqJJwgH19Ff8xKqLPZM9RQ0S3RHR3OKrSkzL8FMvfV5DUfsiQ";
+//		URL url = new URL("http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=Paris%20Hilton");
+//		URLConnection connection = url.openConnection();
+//		connection.addRequestProperty("Referer", "http://voctrl.appspot.com");
+//
+//		String line;
+//		StringBuilder builder = new StringBuilder();
+//		BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+//		while((line = reader.readLine()) != null) {
+//		 builder.append(line);
+//		}
+//
+//		JSONObject json = new JSONObject(builder.toString());
+//
+//		
+//	}
+	
+	
+	
+	
+	
 
 }
