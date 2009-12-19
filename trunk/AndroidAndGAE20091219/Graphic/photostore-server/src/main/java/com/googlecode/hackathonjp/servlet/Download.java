f99f611be71b9f063416ca990ba227546ba4ab85
shin1ogawa <shin1ogawa@bce7ee96-cdb0-11dd-aa7b-9fdd872f0417>
2009-12-19 07:06:36 +0000

    memcache対応した。

--- trunk/AndroidAndGAE20091219/Graphic/photostore-server/src/main/java/com/googlecode/hackathonjp/servlet/Download.java
+++ trunk/AndroidAndGAE20091219/Graphic/photostore-server/src/main/java/com/googlecode/hackathonjp/servlet/Download.java
@@ -11,6 +11,8 @@ import org.apache.commons.lang.StringUtils;
 import org.slim3.datastore.Datastore;
 import org.slim3.datastore.EntityNotFoundRuntimeException;
 
+import appengine.util.MemcacheUtil;
+
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.images.Image;
 import com.google.appengine.api.images.ImagesServiceFactory;
@@ -24,6 +26,11 @@ import com.googlecode.hackathonjp.model.Photo;
 @SuppressWarnings("serial")
 public class Download extends HttpServlet {
 
+	static final String MEMCACHE_SUFFIX_BYTES = "\tBYTES";
+
+	static final String MEMCACHE_SUFFIX_CONTENTTYPE = "\tCONTENTTYPE";
+
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		String keyString = req.getParameter("key");
@@ -31,16 +38,31 @@ public class Download extends HttpServlet {
 			list(req, resp);
 			return;
 		}
+		byte[] bytes = (byte[]) MemcacheUtil.get(keyString + MEMCACHE_SUFFIX_BYTES);
+		String contentType = (String) MemcacheUtil.get(keyString + MEMCACHE_SUFFIX_CONTENTTYPE);
+		if (bytes != null && contentType != null) {
+			// memcacheにhitした！
+			resp.setContentLength(bytes.length);
+			resp.setContentType(contentType);
+			resp.getOutputStream().write(bytes);
+			resp.flushBuffer();
+			return;
+		}
+
 		Key key = Datastore.stringToKey(keyString);
 		try {
 			Photo photo = Datastore.get(Photo.class, key);
 			ServletOutputStream outputStream = resp.getOutputStream();
-			byte[] bytes = photo.getImage();
+			bytes = photo.getImage();
 			resp.setContentLength(bytes.length);
 			Image makeImage = ImagesServiceFactory.makeImage(bytes);
-			resp.setContentType("image/" + makeImage.getFormat().toString().toLowerCase());
+			contentType = "image/" + makeImage.getFormat().toString().toLowerCase();
+			resp.setContentType(contentType);
 			outputStream.write(bytes);
 			resp.flushBuffer();
+			// memcacheに保存しておく。
+			MemcacheUtil.put(keyString + MEMCACHE_SUFFIX_BYTES, bytes);
+			MemcacheUtil.put(keyString + MEMCACHE_SUFFIX_CONTENTTYPE, contentType);
 		} catch (EntityNotFoundRuntimeException e) {
 			resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
 			return;
--- trunk/AndroidAndGAE20091219/Graphic/photostore-server/src/main/java/com/googlecode/hackathonjp/servlet/List.java
+++ trunk/AndroidAndGAE20091219/Graphic/photostore-server/src/main/java/com/googlecode/hackathonjp/servlet/List.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.slim3.datastore.Datastore;
 
+import appengine.util.MemcacheUtil;
+
 import com.googlecode.hackathonjp.meta.PhotoMeta;
 import com.googlecode.hackathonjp.model.Photo;
 
@@ -19,12 +21,19 @@ import com.googlecode.hackathonjp.model.Photo;
 @SuppressWarnings("serial")
 public class List extends HttpServlet {
 
+	static final String MEMCACHE_KEY = "list";
+
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/plain");
 		resp.setCharacterEncoding("utf-8");
-		java.util.List<Photo> list =
-				Datastore.query(PhotoMeta.get()).sort(PhotoMeta.get().createdAt.desc).asList();
+		@SuppressWarnings("unchecked")
+		java.util.List<Photo> list = (java.util.List<Photo>) MemcacheUtil.get(MEMCACHE_KEY);
+		if (list == null) {
+			list = Datastore.query(PhotoMeta.get()).sort(PhotoMeta.get().createdAt.desc).asList();
+			MemcacheUtil.put(MEMCACHE_KEY, list);
+		}
 		for (Photo photo : list) {
 			String keyString = Datastore.keyToString(photo.getKey());
 			resp.getWriter().println(keyString + "\t" + photo.getFilename());
