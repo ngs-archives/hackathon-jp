8e6cb1c4f7c77fc95f1263efcc2bc0ed33febd6a
kazunori279 <kazunori279@bce7ee96-cdb0-11dd-aa7b-9fdd872f0417>
2009-12-19 06:36:18 +0000

    Fixed get bug

--- AndroidAndGAE20091219/Geo/ashiato-server/src/org/hackathon/ashiato/AshiatoPutServlet.java
+++ AndroidAndGAE20091219/Geo/ashiato-server/src/org/hackathon/ashiato/AshiatoPutServlet.java
@@ -13,25 +13,37 @@ import org.slim3.datastore.Datastore;
 import com.google.appengine.api.datastore.GeoPt;
 import com.google.appengine.api.datastore.Transaction;
 
+/**
+ * A servlet that saves specified location data to Datastore.
+ * 
+ * @author kazunori_279
+ */
 public class AshiatoPutServlet extends HttpServlet {
 
+    /**
+     * Saves a location specified by params.
+     */
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse res)
             throws ServletException, IOException {
-
+        
+        // get params
         final String email = req.getParameter("email").toLowerCase().trim();
         final String lng = req.getParameter("lng");
         final String lat = req.getParameter("lat");
 
+        // create new location data
         final Loc loc = new Loc();
         loc.setEmail(email);
         loc.setPoint(new GeoPt(Float.parseFloat(lat), Float.parseFloat(lng)));
         loc.setCreatedAt(new Date());
 
+        // save it
         Transaction tx = Datastore.beginTransaction();
         Datastore.put(loc);
         Datastore.commit(tx);
 
+        // send response
         res.setContentType("text/plain; charset=UTF-8");
         res.getWriter().print("OK");
     }
