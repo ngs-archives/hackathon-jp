b5721b97cde5dd51e6a921e29fa7c57de7e06afe
libido.philadelphia.daisy <libido.philadelphia.daisy@bce7ee96-cdb0-11dd-aa7b-9fdd872f0417>
2009-12-19 05:53:30 +0000

    名前の型を変えたよ！

--- AndroidAndGAE20091219/Sensor/AndroidWarsClient/src/com/example/androidwar/util/Util.java
+++ AndroidAndGAE20091219/Sensor/AndroidWarsClient/src/com/example/androidwar/util/Util.java
@@ -7,42 +7,42 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 
 public class Util {
-	public final static String BR = System.getProperty("line.separator");
+  public final static String BR = System.getProperty("line.separator");
 
-	// ダイアログの表示
-	public static void showOKDialog(final Activity activity, String title,
-			String text) {
-		AlertDialog.Builder ad = new AlertDialog.Builder(activity);
-		ad.setTitle(title);
-		ad.setMessage(text);
-		ad.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog, int whichButton) {
-				activity.setResult(Activity.RESULT_OK);
-			}
-		});
-		ad.create();
-		ad.show();
-	}
-	
-	public static HashMap<String, Object> generateMoveData(int action, String name, int x, int y){
-		HashMap<String, Object> data = new HashMap<String, Object>();
-		data.put("action", action);
-		data.put("name", name);
-		HashMap<String, Object> value = new HashMap<String, Object>(); 
-		value.put("x", x);
-		value.put("y", y);
-		data.put("value", value);
-		return data;
-	}
+  // ダイアログの表示
+  public static void showOKDialog(final Activity activity, String title,
+          String text) {
+      AlertDialog.Builder ad = new AlertDialog.Builder(activity);
+      ad.setTitle(title);
+      ad.setMessage(text);
+      ad.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+          public void onClick(DialogInterface dialog, int whichButton) {
+              activity.setResult(Activity.RESULT_OK);
+          }
+      });
+      ad.create();
+      ad.show();
+  }
+  
+  public static HashMap<String, Object> generateMoveData(int action, String name, int x, int y){
+      HashMap<String, Object> data = new HashMap<String, Object>();
+      data.put("action", action);
+      data.put("name", name);
+      HashMap<String, Object> value = new HashMap<String, Object>(); 
+      value.put("x", x);
+      value.put("y", y);
+      data.put("value", value);
+      return data;
+  }
 
-	public static HashMap<String, Object> generateAtackData(int action, int name, int pawah){
-		HashMap<String, Object> data = new HashMap<String, Object>();
-		data.put("action", action);
-		data.put("name", name);
-		HashMap<String, Object> value = new HashMap<String, Object>(); 
-		value.put("pawah", pawah);
-		data.put("value", value);
-		return data;
-	}
+  public static HashMap<String, Object> generateAtackData(int action, String name, int pawah){
+      HashMap<String, Object> data = new HashMap<String, Object>();
+      data.put("action", action);
+      data.put("name", name);
+      HashMap<String, Object> value = new HashMap<String, Object>(); 
+      value.put("pawah", pawah);
+      data.put("value", value);
+      return data;
+  }
 
 }
