37176eac85540dbf5c3c6e74aa5441a0542a14a2
miyaken5@gmail.com <miyaken5@gmail.com@bce7ee96-cdb0-11dd-aa7b-9fdd872f0417>
2009-12-19 02:41:07 +0000

    プロジェクト登録

---
+++ AndroidAndGAE20091219/Text/voctrl_input/src/jp/hackathon/voctrl/input/voctrl_input.java
@@ -0,0 +1,13 @@
+package jp.hackathon.voctrl.input;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class voctrl_input extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
