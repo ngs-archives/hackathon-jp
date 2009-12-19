8ebea15b9f09e9bf1c7cc0593db973401a3d3af1
hidecheck <hidecheck@bce7ee96-cdb0-11dd-aa7b-9fdd872f0417>
2009-12-19 04:55:43 +0000

    generateDataの戻り値修正

--- trunk/AndroidAndGAE20091219/Sensor/AndroidWarsClient/src/com/example/androidwar/util/Util.java
+++ trunk/AndroidAndGAE20091219/Sensor/AndroidWarsClient/src/com/example/androidwar/util/Util.java
@@ -2,27 +2,16 @@ package com.example.androidwar.util;
 
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
-import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Random;
-
-import org.xmlpull.v1.XmlPullParser;
 
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.util.Log;
-import android.util.Xml;
 
 public class Util {
 	public final static String BR = System.getProperty("line.separator");
-
 
 	// ダイアログの表示
 	public static void showOKDialog(final Activity activity, String title,
@@ -39,7 +28,7 @@ public class Util {
 		ad.show();
 	}
 	
-	public static void generateMoveData(int action, String name, int x, int y){
+	public static HashMap<String, Object> generateMoveData(int action, String name, int x, int y){
 		HashMap<String, Object> data = new HashMap<String, Object>();
 		data.put("action", action);
 		data.put("name", name);
@@ -47,15 +36,17 @@ public class Util {
 		value.put("x", x);
 		value.put("y", y);
 		data.put("value", value);
+		return data;
 	}
 
-	public static void generateAtackData(int action, int name, int power){
+	public static HashMap<String, Object> generateAtackData(int action, int name, int pawah){
 		HashMap<String, Object> data = new HashMap<String, Object>();
 		data.put("action", action);
 		data.put("name", name);
 		HashMap<String, Object> value = new HashMap<String, Object>(); 
-		value.put("power", power);
+		value.put("pawah", pawah);
 		data.put("value", value);
+		return data;
 	}
 
 }
